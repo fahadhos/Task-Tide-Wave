7388f0f0425c81b70db9a9338e6ae66d54af7e0a
fahadhos <generalus159@gmail.com>
2023-08-15 11:08:17 +0600

    fixed: reset button setup done

--- js/index.js
+++ js/index.js
@@ -6,11 +6,11 @@ const Subhanallahbtn = document.getElementById("Subhanallahbtn");
 
 const Alhamdulillahdisplay = document.getElementById("Alhamdulillahdisplay");
 const alhamdulillahbtn = document.getElementById("alhamdulillahbtn");
- 
+
 const allahuakbardisplay = document.getElementById("allahuakbardisplay");
- 
+
 const AllahuAkbarbtn = document.getElementById("AllahuAkbarbtn");
- 
+
 
 const resetbtn = document.getElementById("resetbtn");
 
@@ -20,85 +20,102 @@ console.log(Subhanallahbtn);
 console.log(Alhamdulillahdisplay);
 
 console.log(alhamdulillahbtn);
-let subhanallahdisplayvalue  = 0;
-let alhamdulillahdisplayvalue= 0;
-let allahuakbardisplayvalue  = 0;
+console.log(resetbtn);
+let subhanallahdisplayvalue = 0;
+let alhamdulillahdisplayvalue = 0;
+let allahuakbardisplayvalue = 0;
 
-Subhanallahbtn.addEventListener('click', function() {
+Subhanallahbtn.addEventListener('click', function () {
     subhanallahdisplayvalue += 1;
-    if(subhanallahdisplayvalue >=34){
-    
+    if (subhanallahdisplayvalue >= 34) {
 
-      const successMessage = document.getElementById('message');
-      successMessage.classList.remove('hidden');
-      
+
+        const successMessage = document.getElementById('message');
+        successMessage.classList.remove('hidden');
+
         // Hide the message after 6 seconds (6000 milliseconds)
-        setTimeout(function() {
+        setTimeout(function () {
             successMessage.classList.add('hidden');
         }, 6000);
     }
-    else{
-         subhanallahdisplay.innerText=subhanallahdisplayvalue;
-   
+    else {
+        subhanallahdisplay.innerText = subhanallahdisplayvalue;
+
     }
 
-  
-  
-     console.log(subhanallahdisplayvalue)
+
+
+    console.log(subhanallahdisplayvalue)
 });
 // end of subhanallahdisplayvalue
 
 // Alhamdulillah Update er code
-alhamdulillahbtn.addEventListener('click', function() {
+alhamdulillahbtn.addEventListener('click', function () {
     alhamdulillahdisplayvalue += 1;
-    if(alhamdulillahdisplayvalue >=34){
-    
+    if (alhamdulillahdisplayvalue >= 34) {
 
-      const successMessage = document.getElementById('message2');
-      successMessage.classList.remove('hidden');
-      
+
+        const successMessage = document.getElementById('message2');
+        successMessage.classList.remove('hidden');
+
         // Hide the message after 6 seconds (6000 milliseconds)
-        setTimeout(function() {
+        setTimeout(function () {
             successMessage.classList.add('hidden');
         }, 6000);
     }
-    else{
-         Alhamdulillahdisplay.innerText=alhamdulillahdisplayvalue;
-   
+    else {
+        Alhamdulillahdisplay.innerText = alhamdulillahdisplayvalue;
+
     }
-  
+
     console.log(Alhamdulillahdisplay);
-   
+
 });
 
 // Allahuakbar Update er code
-AllahuAkbarbtn.addEventListener('click', function() {
+AllahuAkbarbtn.addEventListener('click', function () {
     allahuakbardisplayvalue += 1;
-   
-    if(allahuakbardisplayvalue >=35){
-       
-    
-      const successMessage = document.getElementById('message3');
-      successMessage.classList.remove('hidden');
-       // Hide the message after 6 seconds (6000 milliseconds)
-        setTimeout(function() {
+
+    if (allahuakbardisplayvalue >= 35) {
+
+
+        const successMessage = document.getElementById('message3');
+        successMessage.classList.remove('hidden');
+        // Hide the message after 6 seconds (6000 milliseconds)
+        setTimeout(function () {
             successMessage.classList.add('hidden');
         }, 6000);
-      const showModal = document.getElementById('modal');
-      showModal.classList.remove('hidden');
-      
-       // Hide the Modal dua button after 6 seconds (6000 milliseconds)
-       setTimeout(function() {
-        showModal.classList.add('hidden');
-    }, 6000);
+        const showModal = document.getElementById('modal');
+        showModal.classList.remove('hidden');
+
+        // Hide the Modal dua button after 6 seconds (6000 milliseconds)
+        setTimeout(function () {
+            showModal.classList.add('hidden');
+        }, 6000);
 
     }
-    else{
-         allahuakbardisplay.innerText= allahuakbardisplayvalue;
-   
+    else {
+        allahuakbardisplay.innerText = allahuakbardisplayvalue;
+
     }
 
     console.log(allahuakbardisplayvalue);
-   
+
 });
- 
+
+
+
+
+
+// reset button stablished
+
+resetbtn.addEventListener('click', function () {
+    subhanallahdisplayvalue = 0;
+    alhamdulillahdisplayvalue = 0;
+    allahuakbardisplayvalue = 0;
+    subhanallahdisplay.innerText = subhanallahdisplayvalue;
+    Alhamdulillahdisplay.innerText = alhamdulahdisplay;
+    allahuakbardisplay.innerText = allahuakbardisplayvalue;
+
+
+});

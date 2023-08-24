44e008081ad4b1f38841791df37617b455105619
fahadhos <generalus159@gmail.com>
2023-08-24 20:16:09 +0600

    Upgraded: Fixed  vibratation duration on mobile 24 Aug

--- js/index.js
+++ js/index.js
@@ -53,7 +53,7 @@ Subhanallahbtn.addEventListener('click', function () {
     }
     if ("vibrate" in navigator) {
         // Vibrate for 500 milliseconds
-        navigator.vibrate(500);
+        navigator.vibrate(100);
       } else {
         console.log("Vibration not supported on this device.");
       }
@@ -89,7 +89,7 @@ alhamdulillahbtn.addEventListener('click', function () {
     console.log(Alhamdulillahdisplay);
     if ("vibrate" in navigator) {
         // Vibrate for 500 milliseconds
-        navigator.vibrate(500);
+        navigator.vibrate(100);
       } else {
         console.log("Vibration not supported on this device.");
       }
@@ -130,7 +130,7 @@ AllahuAkbarbtn.addEventListener('click', function () {
     console.log(allahuakbardisplayvalue);
     if ("vibrate" in navigator) {
         // Vibrate for 500 milliseconds
-        navigator.vibrate(500);
+        navigator.vibrate(100);
       } else {
         console.log("Vibration not supported on this device.");
       }
